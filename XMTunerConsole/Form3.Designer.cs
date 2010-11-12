694ece583835c05fe34c362d019ac4f482da9333
Chris Crews <80741868+PsychoticWolf@users.noreply.github.com>
2010-11-12 05:14:26 +0000

    * Don't generate members for Form1 controls we aren't touching

--- XMTunerConsole/Form3.Designer.cs
+++ XMTunerConsole/Form3.Designer.cs
@@ -122,6 +122,7 @@
             // 
             // Form3
             // 
+            this.AcceptButton = this.bClose;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(559, 146);
