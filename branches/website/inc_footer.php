2ec4a07d34bede75fc40dbb6e77b58bedf5c5ff8
nobody <nobody@89476c66-db73-11de-9f0a-59c23b477afa>
2011-06-30 03:03:03 +0000

    * Added Paypal donation button * Updated notice for SiriusXM outage for WMA streams

--- branches/website/inc_footer.php
+++ branches/website/inc_footer.php
@@ -1,5 +1,26 @@
 <div id="footer">
+	<?php
+	if ($skip_donate != true)
+	{
+	?>
+	<div style="float: left; padding-left: 20px; color: #666; font-size: 8pt;">
+	Want to Help Support XMTuner's Development?
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="padding-left: 10px;">
+		<input type="hidden" name="cmd" value="_s-xclick">
+		<input type="hidden" name="hosted_button_id" value="FYBREFY97BCVL">
+		<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110429-1/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+		<img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110429-1/en_US/i/scr/pixel.gif" width="1" height="1">
+	</form>
+	
+	</div>
+	<?php
+	}
+	?>
+
 	<div id="footer-copy">XMTuner created by <a href="http://www.psychoticwolf.net/" target="_blank">Chris Crews</a> and <a href="" target="_blank">Curtis M. Kularski</a><br>&copy; 2009-2010 <a href="http://www.pcfire.net/" target="_blank">PCFire</a> | Not affiliated with <a href="http://www.siriusxm.com/" target="_blank">SiriusXM Satelite Radio Inc</a>.</div>
+	
+	
+
 </div>
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
