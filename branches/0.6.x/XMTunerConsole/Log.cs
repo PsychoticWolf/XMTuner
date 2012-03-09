63d64c38b91bb63b8496dbc206a55c8675b0a865
nobody <nobody@89476c66-db73-11de-9f0a-59c23b477afa>
2012-03-09 00:58:39 +0000

    XMTuner 0.6.x is now licensed under the GPLv3.

--- branches/0.6.x/XMTunerConsole/Log.cs
+++ branches/0.6.x/XMTunerConsole/Log.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * XMTuner: Copyright (C) 2009-2012 Chris Crews and Curtis M. Kularski.
+ * 
+ * This file is part of XMTuner.
+
+ * XMTuner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * XMTuner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with XMTuner.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.IO;
 using System.Windows.Forms;
 
