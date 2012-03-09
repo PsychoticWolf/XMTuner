fc0bd944ca50fc321e71b2e2c1505d8305a48150
nobody <nobody@89476c66-db73-11de-9f0a-59c23b477afa>
2012-03-09 02:48:22 +0000

    Add Thank You page for donators (wasn't in VC yet.)

---
+++ branches/website/thankyou/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<!-- Created Sun May 30 18:27:36 2010 -->
+<html>
+<head>
+<link rel="stylesheet" href="../xmtuner.css" type="text/css">
+
+<title>XMTuner :: Thank you for your donation!</title>
+</head>
+<body>
+<?php
+	include_once"../inc_superbar.php";
+	include_once"../inc_header.php";
+?>
+
+<div id="body">
+
+<h2 class="red">Thank You for your donation!</h2>
+<p>Thank you for donating to the XMTuner project.</p>
+
+<?php
+	$skip_donate = true;
+	include_once"../inc_footer.php";
+?>
+</body>
+</html>
