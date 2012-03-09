0ccd9bb76065cc8af20957acefa619e9928735ef
nobody <nobody@89476c66-db73-11de-9f0a-59c23b477afa>
2012-03-09 02:01:16 +0000

    * Add my superbar. * Fix z-index issue on blog - floating nav hides blog admin bar.

--- branches/website/inc_superbar.php
+++ branches/website/inc_superbar.php
@@ -1,3 +1,6 @@
+<link rel="stylesheet" type="text/css" href="http://xmtuner.net/xmtuner.css" media="screen" />
+
+<?php include_once"D:/web/net/psychoticwolf/pw_global_header.php"; ?>
 <div id="superbar">
 <a href="http://www.xmtuner.net/">XMTuner</a> | <a href="http://blog.xmtuner.net/">Blog</a> | <a href="http://wiki.xmtuner.net">Support</a> | <a href="http://sourceforge.net/projects/xmtuner/forums/forum/1050609" target="_blank">Forum</a> | <a href="http://www.xmtuner.net/contact/">Contact</a>
 </div>
